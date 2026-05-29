215d1b382ee508460090573a86fa9cddd66e70df
Murilo Jochkeck <murilojochkeck@gmail.com>
2026-05-29 08:22:09 -0300

    projeto rodando

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,11 +2,11 @@ export {
   assertCanActAs,
   type AuthContext,
   type ProfileRoleFlags,
-} from "./identity/roles";
+} from "./identity/roles.js";
 export {
   profileRoleFlagsSchema,
   type ProfileRoleFlagsInput,
-} from "./identity/schemas";
+} from "./identity/schemas.js";
 
-export * from "./demands/schemas";
-export * from "./discovery/schemas";
+export * from "./demands/schemas.js";
+export * from "./discovery/schemas.js";
